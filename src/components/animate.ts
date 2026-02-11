f4a123854b61cbe5498bca15cc729a8556081cf8
Piotr Proc <piotr.proc@gmail.com>
2026-02-11 13:23:04 +0100

    Animate cards mockup

--- src/components/animate.ts
+++ src/components/animate.ts
@@ -8,7 +8,7 @@ export function animateCards(app:Application, cardsOnStacks: Sprite[][]) {
 
     createRotationAnimation(app, cardsOnStacks[0][CARDS_IN_COLUMN-1])
     createRotationAnimation(app, cardsOnStacks[1][CARDS_IN_COLUMN-1])
-    createRotationAnimation(app, cardsOnStacks[2][CARDS_IN_COLUMN-1])
+    createRotationAnimationLastColumn(app, cardsOnStacks[2][CARDS_IN_COLUMN-1])
 }
 
 
@@ -19,6 +19,8 @@ export function moveAllCardsExceptOne(cards: Sprite[]) {
 }
 
 export function createRotationAnimation(app:Application, sprite: Sprite) {
+    sprite.zIndex = -1;
+
     let offsetX = 0;
     let offsetY = 0;
 
@@ -32,6 +34,32 @@ export function createRotationAnimation(app:Application, sprite: Sprite) {
         if(offsetY < 480) {
             sprite.y -= 15;
             offsetY += 15;
+        }
+    });
+    ticker.start();
+
+    setTimeout(function () {
+        ticker.stop()
+        sprite.rotation = 0;
+    }, 2000)
+}
+
+export function createRotationAnimationLastColumn(app:Application, sprite: Sprite) {
+    sprite.zIndex = -1;
+
+    let offsetX = 0;
+    let offsetY = 0;
+
+    const ticker = new Ticker();
+    ticker.add(() => {
+        if(offsetX < 600) {
+            sprite.x -= 10;
+            offsetX += 10;
+        }
+
+        if(offsetY < 480) {
+            sprite.y -= 8;
+            offsetY += 8;
         }
     });
     ticker.start();
