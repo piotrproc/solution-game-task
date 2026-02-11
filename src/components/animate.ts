38275ca3cc129e7b58b2a6fc4da5a6d915c2fba8
Piotr Proc <piotr.proc@gmail.com>
2026-02-11 18:35:59 +0100

    Animate cards 2

--- src/components/animate.ts
+++ src/components/animate.ts
@@ -1,26 +1,26 @@
 import { Application, Sprite, Ticker } from "pixi.js";
-import { ACE_ANIMATION_TIME, CARDS_IN_COLUMN, DECK_HEIGHT, TO_DIFFERENT_STACK_TIME } from "./globalVariables/consts.ts";
+import { SHIFT_ANIMATION_TIME, CARDS_IN_COLUMN, DECK_HEIGHT, ROUND_TIME } from "./globalVariables/consts.ts";
 
 export function animateCardsInLoop(app: Application, cardsOnStacks: Sprite[][]) {
     for (let i = 0; i < 5; i++) {
         setTimeout(() => {
-            animateCards(app, cardsOnStacks, i);
-        }, i * TO_DIFFERENT_STACK_TIME);
+            animateCards(app, cardsOnStacks);
+        }, i * ROUND_TIME);
     }
 }
 
-export function animateCards(app: Application, cardsOnStacks: Sprite[][], shuffleNumber: number) {
-    moveAllCardsExceptOne(cardsOnStacks[0])
-    moveAllCardsExceptOne(cardsOnStacks[1])
-    moveAllCardsExceptOne(cardsOnStacks[2])
+export function animateCards(app: Application, cardsOnStacks: Sprite[][]) {
+    moveAllCardsExceptOne(cardsOnStacks[0], 0)
+    moveAllCardsExceptOne(cardsOnStacks[1], 1)
+    moveAllCardsExceptOne(cardsOnStacks[2], 2)
 
     const lastElement1 = cardsOnStacks[0].pop() as Sprite;
     const lastElement2 = cardsOnStacks[1].pop() as Sprite;
     const lastElement3 = cardsOnStacks[2].pop() as Sprite;
 
-    createRotationAnimation(app, lastElement1, shuffleNumber);
-    createRotationAnimation(app, lastElement2, shuffleNumber);
-    createRotationAnimationLastColumn(app, lastElement3, shuffleNumber);
+    createRotationAnimation(app, lastElement1, 0);
+    createRotationAnimation(app, lastElement2, 1);
+    createRotationAnimationLastColumn(app, lastElement3);
 
     cardsOnStacks[0].unshift(lastElement3);
     cardsOnStacks[1].unshift(lastElement1);
@@ -28,14 +28,15 @@ export function animateCards(app: Application, cardsOnStacks: Sprite[][], shuffl
 }
 
 
-export function moveAllCardsExceptOne(cards: Sprite[]) {
+export function moveAllCardsExceptOne(cards: Sprite[], stackIndex: number) {
     cards.forEach((card) => {
         card.y += 10;
+        card.zIndex = stackIndex * 2;
     })
 }
 
-export function createRotationAnimation(app: Application, sprite: Sprite, shuffleNumber:number) {
-    sprite.zIndex = -1 * shuffleNumber - 1;
+export function createRotationAnimation(app: Application, sprite: Sprite, stackIndex:number) {
+    sprite.zIndex = stackIndex * 2 + 1;
 
     let offsetX = 0;
     let offsetY = 0;
@@ -57,11 +58,11 @@ export function createRotationAnimation(app: Application, sprite: Sprite, shuffl
 
     setTimeout(function () {
         ticker.stop()
-    }, ACE_ANIMATION_TIME)
+    }, SHIFT_ANIMATION_TIME)
 }
 
-export function createRotationAnimationLastColumn(app: Application, sprite: Sprite, shuffleNumber: number) {
-    sprite.zIndex = -1 * shuffleNumber - 1;
+export function createRotationAnimationLastColumn(app: Application, sprite: Sprite) {
+    sprite.zIndex = -1;
 
     let offsetX = 0;
     let offsetY = 0;
@@ -82,5 +83,5 @@ export function createRotationAnimationLastColumn(app: Application, sprite: Spri
 
     setTimeout(function () {
         ticker.stop()
-    }, ACE_ANIMATION_TIME)
+    }, SHIFT_ANIMATION_TIME)
 }
