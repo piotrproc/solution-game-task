ba34ed3fc70444e6d7ac2d8dd8ba78da2921a9bb
Piotr Proc <piotr.proc@gmail.com>
2026-02-14 16:36:12 +0100

    Quick adjust to mobile

--- src/components/gui/consts.ts
+++ src/components/gui/consts.ts
@@ -1,7 +1,8 @@
 import { isMobile } from "pixi.js";
+import { isPortrait } from "../utils.ts";
 
 export const TASK_TEXTS = [
-    isMobile.any ? "1st task" : "1st assignment",
-    isMobile.any ? "2st task" : "2st assignment",
-    isMobile.any ? "3st task" : "3st assignment"
+    isMobile.any && isPortrait() ? "1st task" : "1st assignment",
+    isMobile.any && isPortrait() ? "2nd task" : "2nd assignment",
+    isMobile.any && isPortrait() ? "3rd task" : "3rd assignment"
 ];
